15259b8c8f7abd6863e9a86c7c5f4ec166f1f011
Jayesh Shere <jshere@expedia.com>
2017-06-16 23:52:38 +1000

    refactor

--- src/main/kotlin/org/js/org.js.Parser.kt
+++ src/main/kotlin/org/js/org.js.Parser.kt
@@ -1,11 +1,8 @@
 package org.js
 
 import org.parboiled.BaseParser
-import org.parboiled.Parboiled
 import org.parboiled.Rule
 import org.parboiled.annotations.BuildParseTree
-import org.parboiled.parserunners.ReportingParseRunner
-import org.parboiled.support.ParseTreeUtils
 
 @BuildParseTree
 open class Parser : BaseParser<Expression>() {
@@ -75,22 +72,4 @@ open class Parser : BaseParser<Expression>() {
     open fun WhiteSpace(): Rule {
         return ZeroOrMore(AnyOf(" \t"))
     }
-}
-
-fun run(): Unit {
-    val input: String = "p3:123"
-    val input1: String = "userId:123,2123,3111 or (lang:en_US,en_UK and p4:1%)"
-
-    val prsr: Parser = Parboiled.createParser(Parser::class.java)
-    val reportingParseRunner = ReportingParseRunner<Expression>(prsr.Expr())
-    val result = reportingParseRunner.run(input)
-    if (result.hasErrors()) println(result.parseErrors)
-    print(ParseTreeUtils.printNodeTree(result))
-
-    val evalResult = result.resultValue.eval(linkedMapOf(Pair("p1", "foo"), Pair("p3", "1asdfadadfadfa")))
-    println("Eval result: $evalResult")
-}
-
-fun main(args: Array<String>) {
-    run()
 }
